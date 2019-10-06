cce0ef0126214c49cd46e44250fcae305de859c6
Alyssa Sygrove <alyssa@DESKTOP-SENQP7M>
2019-10-05 18:56:41 -0700

    Initial AddingMachine file without any changes.

---
+++ src/cse360assign2/AddingMachine.java
@@ -0,0 +1,63 @@
+/**
+ * Author: Alyssa Sygrove
+ * Class #: 70642
+ * ASU ID: 1212579574
+ * 
+ * This file does basic arithmetic operations on numbers to find the total 
+ * and return a string of the operations.The majority of the methods 
+ * are purposefully left blank.
+ */
+package cse360assign2;
+/**
+ * Contains methods that perform basic arithmetic, finds the total, 
+ * and displays the string of operations.
+ */
+public class AddingMachine {
+	
+	private int total;
+	
+	/**
+	 * Initiates the total as 0.
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total as an integer.
+	 * @return the total from performing operations
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds the parameter to the total.
+	 * @param value is the number to be added to the total.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts the parameter to the total.
+	 * @param value is the number to be subtracted from the total.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Returns a string of the operations done.
+	 */
+	public String toString () {
+		return "";
+		}
+	
+	/**
+	 * Clears the total value and the history.
+	 */
+	public void clear() {
+		
+	}
+}
